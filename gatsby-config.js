ab60e7bfdc611522b7e6d5fecb9f62516be3cc54
Valentin Vago <valentin.vago@gmail.com>
2018-09-11 16:16:53 +0200

    add some plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,40 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'TopLap Berlin',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/posts/`,
+        name: 'posts',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-plugin-sharp',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 800,
+              backgroundColor: 'transparent',
+            },
+          },
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+  ],
 }
